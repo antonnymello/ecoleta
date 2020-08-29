0a7b88ebaa0184ed7f902d211a93438b6a0af4b3
antonnymello <antonny.mello@gmail.com>
2020-08-29 09:34:02 -0300

    Responsiveness improved. Some DB alterations.

--- src/database/db.js
+++ src/database/db.js
@@ -59,8 +59,8 @@ db.serialize(() => {
   //   console.log('Aqui estão seus registros!');
   //   console.log(rows);
   // });
-  //   //4. Deletar um dado da tabela
-  // db.run(`DELETE FROM places WHERE id = ?`, [7], function (err) {
+  // //4. Deletar um dado da tabela
+  // db.run(`DELETE FROM places WHERE id = ?`, [17], function (err) {
   //   if (err) {
   //     return console.log(err);
   //   }
